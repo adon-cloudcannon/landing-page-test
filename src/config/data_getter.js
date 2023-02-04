d520df2c22bb35df925514b6d444a432735b41b5
Mike Neumegen <mike@cloudcannon.com>
2023-02-03 16:51:08 -0800

    fix plugins

---
+++ src/config/data_getter.js
@@ -0,0 +1,14 @@
+const EleventyFetch = require("@11ty/eleventy-fetch"); // here to keep bookshop happy
+
+module.exports = {
+	download_github_readme: async function(repo, branch, readme) {
+		let githubFileOrigin = repo.replace(/https\:\/\/github\.com\//, 'https://raw.githubusercontent.com/'),
+		readmeUrl = `${githubFileOrigin}/${branch}/${readme}`;
+		console.log(`Downloading ${readmeUrl}...`);
+
+		return EleventyFetch(readmeUrl, {
+			duration: "1d",
+			type: "text"
+		});
+	}
+};
--- src/config/helpers.js
+++ src/config/helpers.js
@@ -1,6 +1,5 @@
 // const crypto = require('crypto');
 
-
 module.exports = {
 	excerpt: function(post) {
 		const content = post.replace(/(<([^>]+)>)/gi, "");
@@ -10,18 +9,5 @@ module.exports = {
 	uuid: function(name) {
 		for (var s=''; s.length < 20; s += 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'.charAt(Math.random()*62|0));
 		return name + "-" + s;
-	},
-
-	download_github_readme: async function(repo, branch, readme) {
-
-		let EleventyFetch = require("@11ty/eleventy-fetch"); // here to keep bookshop happy
-		let githubFileOrigin = repo.replace(/https\:\/\/github\.com\//, 'https://raw.githubusercontent.com/'),
-		readmeUrl = `${githubFileOrigin}/${branch}/${readme}`;
-		console.log(`Downloading ${readmeUrl}...`);
-
-		return EleventyFetch(readmeUrl, {
-			duration: "1d",
-			type: "text"
-		});
 	}
 };
