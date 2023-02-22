f466004bb7ad7c706ebc95ab7b4298ca562cd928
Mike Neumegen <mike@cloudcannon.com>
2023-02-21 22:02:29 -0800

    fixes

--- src/collections/lessons/lessons.11tydata.js
+++ src/collections/lessons/lessons.11tydata.js
@@ -2,7 +2,7 @@ module.exports = {
 	tags: ["lessons"],
 	layout: "lesson.html",
 	permalink: data => {
-		let base = "/learn/" + data.tutorial + "/";
+		let base = "/tutorials/" + data.tutorial + "/";
 		if (data.order != 1 && data.order != '1') {
 			base += data.page.fileSlug + "/";
 		}
