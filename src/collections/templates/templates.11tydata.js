4d21050f6a9e5202af6a79c5e2207e4b122529f9
Mike Neumegen <mike@cloudcannon.com>
2023-03-06 06:10:35 -0800

    add template

--- src/collections/templates/templates.11tydata.js
+++ src/collections/templates/templates.11tydata.js
@@ -2,5 +2,8 @@ module.exports = {
 	layout: "template.html",
 	lead_score: 1,
 	tags: ["templates"],
-	permalink: "/templates/{{ page.fileSlug }}/"
+	permalink: "/templates/{{ page.fileSlug }}/",
+	eleventyComputed: {
+		title: data => data.name
+	}
 };
