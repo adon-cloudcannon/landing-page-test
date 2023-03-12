9d2ca15ca20bcd9476d22f60727bc80430d221f5
Mike Neumegen <mike@cloudcannon.com>
2023-03-11 19:34:46 -0800

    trying to fix performance issuese with Hubspot

--- src/main.js
+++ src/main.js
@@ -90,6 +90,31 @@ let urls = [];
 	});
 });
 
+/* script load delay logic */
+
+const autoLoadDuration = 5; //In Seconds
+const eventList = ["keydown", "mousemove", "wheel", "touchmove", "touchstart", "touchend"];
+
+const autoLoadTimeout = setTimeout(runScripts, autoLoadDuration * 1000);
+
+eventList.forEach(function(event) {
+		window.addEventListener(event, triggerScripts, { passive: true })
+});
+
+function triggerScripts() {
+		runScripts();
+		clearTimeout(autoLoadTimeout);
+		eventList.forEach(function(event) {
+				 window.removeEventListener(event, triggerScripts, { passive: true });
+		});
+}
+
+function runScripts() {
+		document.querySelectorAll("script[delay]").forEach(function(scriptTag) {
+				scriptTag.setAttribute("src", scriptTag.getAttribute("delay"));
+		});
+}
+
 /* Pagefind implementation */
 
 // TODO: Replace with https://github.com/CloudCannon/pagefind/issues/226
