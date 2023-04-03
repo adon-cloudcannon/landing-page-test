ca9e5d2f473313c31e5bd7f6eab4f291f0f48437
tomrcc <tom@cloudcannon.com>
2023-04-03 14:23:35 +1200

    Fix: change color options for icons

--- src/styles/tailwind.config.js
+++ src/styles/tailwind.config.js
@@ -35,7 +35,13 @@ module.exports = {
 				sky: '#b4e7f7',
 				ash: '#f7f5f2',
 				night: '#2B2B31',
-				midnight: '#26252A'
+				midnight: '#26252A',
+				cc_blue: '#034AD8',
+				amber: "#FC6D26",
+				electric_purple: "#C703D8",
+				emerald: "#38A413",
+				ruby: "#F03A47",
+				comet: "#8BB4ED"
 			},
 			boxShadow: {
 				'menu': '0 5px 25px 10px rgb(0 0 0 / 0.08)'
