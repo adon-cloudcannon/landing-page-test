5c2c5448086b5083974e645125e95173962ef396
Adon Moskal <adonmoskal@Adons-MBP.cloudcannon.office>
2024-01-04 08:20:03 +1300

    Refactor: remove assign_local... used to test a bug/fixed

--- .eleventy.js
+++ .eleventy.js
@@ -124,22 +124,6 @@ module.exports = function (eleventyConfig) {
 
 	eleventyConfig.addFilter("markdownify", (markdown) => md.render(markdown));
 
-	eleventyConfig.addLiquidTag('assign_local', liquidEngine => {
-		return {
-		  parse: function (token) {
-			  const tokenizer = new Tokenizer(token.args, this.liquid.options.operatorsTrie);
-			  this.key = tokenizer.readIdentifier().content;
-			  tokenizer.skipBlank();
-			  assert(tokenizer.peek() === '=', () => `illegal token ${token.getText()}`);
-			  tokenizer.advance();
-			  this.value = tokenizer.remaining();
-		  },
-		  render: function(ctx) {
-			  ctx.scopes[ctx.scopes.length-1][this.key] = this.liquid.evalValueSync(this.value, ctx);
-		  }
-		}
-	});
-
 	return {
 		dir: {
 			input: "src",
